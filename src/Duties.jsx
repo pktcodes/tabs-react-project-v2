ff447387990843b691608d67fa6cae0a0af8ec38
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-19 23:53:01 -0500

    Duties and UUID Library

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,6 +1,22 @@
-const Duties = (duties) => {
-  console.log(duties);
-  return <h2>duties</h2>;
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { v4 as uuidv4 } from 'uuid';
+
+const Duties = ({ duties }) => {
+  return (
+    <>
+      {duties.map((duty, index) => {
+        const id = uuidv4();
+        console.log(id);
+
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Duties;
